252e609a8a84da472d574763b536859e0773090a
AnnaKostyanaya <34882411+AnnaKostyanaya@users.noreply.github.com>
2023-03-13 18:06:14 +0000

    Deploying to gh-pages from @ AnnaKostyanaya/goit-react-hw-08-phonebook@4d8c8b9d9f3a5e9139f1f52c6fd3c52a915439e1 🚀

--- static/js/936.590fa085.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,t,r){r.r(t),r.d(t,{default:function(){return i}});var c=r(3728),a=r(9983),o="Home_chapter__t5ojl",s="Home_text__YlQ2v",n="Home_icon__qpU8J",l=r(184),i=function(){return(0,l.jsxs)("div",{className:o,children:[(0,l.jsx)("p",{className:s,children:"Create your own contact book!"}),(0,l.jsx)(a.Pd.Provider,{value:{size:"10em"},children:(0,l.jsx)("div",{className:n,children:(0,l.jsx)(c.efA,{})})})]})}}}]);
-//# sourceMappingURL=936.590fa085.chunk.js.map
